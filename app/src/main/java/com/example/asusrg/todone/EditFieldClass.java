0df3b6c8272180987531a78b1ce093a65d96a0ab
Kaamil17 <ehsankamil6@gmail.com>
2018-08-31 23:51:21 +0300

    New Changes.

--- app/src/main/java/com/example/asusrg/todone/EditFieldClass.java
+++ app/src/main/java/com/example/asusrg/todone/EditFieldClass.java
@@ -1,6 +1,11 @@
 package com.example.asusrg.todone;
 
+import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,16 +13,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
 
-public class EditFieldClass extends AppCompatActivity {
+public class EditFieldClass extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
     EditText editText;
-    Button buttonImage;
     TextView calenderID;
+    TextView timeText;
+
     DatePickerDialog datePickerDialog;
+    Button buttonImage;
+    Button clockButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,7 @@ public class EditFieldClass extends AppCompatActivity {
 
         calenderID = findViewById(R.id.calenderID);
         buttonImage = findViewById(R.id.ButtonImage);
+        clockButton = findViewById(R.id.clockButton);
 
         buttonImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +46,7 @@ public class EditFieldClass extends AppCompatActivity {
                 final Calendar calender = Calendar.getInstance();
                 int year = calender.get(Calendar.YEAR);
                 int month = calender.get(Calendar.MONTH);
-                final int day = calender.get(Calendar.DAY_OF_MONTH);
+                int day = calender.get(Calendar.DAY_OF_MONTH);
 
                 datePickerDialog = new DatePickerDialog(EditFieldClass.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
@@ -53,6 +65,14 @@ public class EditFieldClass extends AppCompatActivity {
 
         });
 
+        clockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EditFieldClass.this, "Not Ready Yet!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
 
@@ -68,6 +88,25 @@ public class EditFieldClass extends AppCompatActivity {
             setResult(Intent_Constants.INTENT_RESULT_CODE, intent);
             finish();
         }
+    }
+
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+        updateTimeText(calendar);
+      // startAlarm(calendar);
+
+    }
+
+    public void updateTimeText(Calendar calendar) {
+        timeText = findViewById(R.id.timeText);
+        String textForTime = "Alarm set for: ";
+        textForTime += DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.getTime());
+        timeText.setText(textForTime);
     }
 
 }
--- app/src/main/java/com/example/asusrg/todone/MainActivity.java
+++ app/src/main/java/com/example/asusrg/todone/MainActivity.java
@@ -1,63 +1,168 @@
 package com.example.asusrg.todone;
 
-import android.app.DatePickerDialog;
+
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
-import android.widget.ImageView;
+
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.text.DateFormat;
+
 import java.util.ArrayList;
-import java.util.Calendar;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
+    SwipeMenuListView listView;
     ArrayList<String> arrayList;
     ArrayAdapter<String> arrayAdapter;
     String messageText;
     int position;
+    EditText taskMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
+        taskMessage = findViewById(R.id.taskMessage);
+
+
 
         arrayList = new ArrayList<>();
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(arrayAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        // For swiping ListView.
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent();
-                intent.setClass(getApplicationContext(), EditMessage.class);
-                intent.putExtra(Intent_Constants.INTENT_MESSAGE_DATA, arrayList.get(position));
-                intent.putExtra(Intent_Constants.INTENT_ITEM_POSITION, position);
-                startActivityForResult(intent, Intent_Constants.INTENT_REQUEST_CODE_TWO);
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(170);
+                // set item title
+                openItem.setTitle("Edit");
+                // set item title font size
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_delete_black_24dp);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+
+                // swipe start
+                SwipeMenuItem doneItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item Background
+                doneItem.setBackground(new ColorDrawable(Color.rgb(0x25,
+                        0x3F, 0x25)));
+                //
+                doneItem.setWidth(150);
+                // set an icon
+                doneItem.setIcon(R.drawable.ic_done_black_24dp);
+                // add to menu
+                menu.addMenuItem(doneItem);
+
+            }
+        };
+
+        // set creator
+        listView.setMenuCreator(creator);
+
+
+        // specifying what left and right swipe should do.
+        listView.setOnSwipeListener(new SwipeMenuListView.OnSwipeListener() {
+
+            @Override
+            public void onSwipeStart(int position) {
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeEnd(int position) {
+                // swipe end
             }
         });
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+
+                        Toast.makeText(MainActivity.this, "Editing Task", Toast.LENGTH_SHORT).show();
+                        //   ItemClickListening Method
+                        Intent intent = new Intent();
+                        intent.setClass(getApplicationContext(), EditMessage.class);
+                        intent.putExtra(Intent_Constants.INTENT_MESSAGE_DATA, arrayList.get(position));
+                        intent.putExtra(Intent_Constants.INTENT_ITEM_POSITION, position);
+                        startActivityForResult(intent, Intent_Constants.INTENT_REQUEST_CODE_TWO);
+
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "Deleting Task", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+
+                }
+                return true;
+            }
+        });
+
 
         try {
             Scanner scanner = new Scanner(openFileInput("Todo.txt"));
             while (scanner.hasNextLine()) {
                 String data = scanner.nextLine();
                 arrayAdapter.add(data);
+
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+
     }
+
 
     @Override
     public void onBackPressed() {
@@ -72,6 +177,7 @@ public class MainActivity extends AppCompatActivity {
         }
         finish();
     }
+
 
     public void floatingButtonClickListener(View view) {
         Intent intent = new Intent();
@@ -96,4 +202,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
 }
